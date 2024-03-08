b4765090ab4755baa7deb8cc9943b2085adf41ea
RudolfsTaurins <s22sprokate@venta.lv>
2024-03-08 14:36:59 +0200

    Add: Arrays implemented

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -6,37 +6,99 @@ import model.Grade;
 import model.Professor;
 import model.Student;
 
+
+import java.util.ArrayList;
+
+
+
 public class MainService {
+	
+	private static ArrayList<Professor> allProfessors = new ArrayList<>();
+	private static ArrayList<Student> allStudents = new ArrayList<>();
+	private static ArrayList<Course> allCourses = new ArrayList<>();
+	private static ArrayList<Grade> allGrades = new ArrayList<>();
+	
 	public static void main(String[] args) {
 		Professor pr1 = new Professor();
-		System.out.println(pr1);
+		allProfessors.add(pr1);
 		Professor pr2 = new Professor("Karina", "Skirmante", Degree.mg);
-		System.out.println(pr2);
-		
+		allProfessors.add(pr2);
 		Professor pr3 = new Professor("Marcis", "Naktins", Degree.mg);
-		System.out.println(pr3);
+		allProfessors.add(pr3);
+		Professor pr4 = new Professor("Estere", "Vitola", Degree.mg);
+		allProfessors.add(pr4);
+		
+		//TODO take a look at ArrayList class
+		for(int i = 0; i < allProfessors.size(); i++) {
+			System.out.println(allProfessors.get(i));
+		}
+		System.out.println("------------------------------------------");
+		
+		
+		
 		
 		Student st1 = new Student();
-		System.out.println(st1);
+		allStudents.add(st1);
 		Student st2 = new Student("Annija", "Aumale");
-		System.out.println(st2);
+		allStudents.add(st2);
+		Student st3 = new Student("Lara", "Bernardes");
+		allStudents.add(st3);
+		
+		for(Student tempSt : allStudents) {
+			System.out.println(tempSt);
+		}
+		System.out.println("------------------------------------------");
 		
 		Course c1 = new Course();
-		System.out.println(c1);
+		allCourses.add(c1);
 		Course c2 = new Course("JAVA", 4, pr2);
-		System.out.println(c2);
-		
+		allCourses.add(c2);
 		Course c3 = new Course("Networking", 2, pr3);
-		System.out.println(c3);
+		allCourses.add(c3);
+		Course c4 = new Course("Data Structures", 2, pr4);
+		allCourses.add(c4);
+		
+		for(Course tempC : allCourses) {
+			System.out.println(tempC);
+		}
+		System.out.println("------------------------------------------");
 		
 		Grade g1 = new Grade();
-		System.out.println(g1);
-		
+		allGrades.add(g1);
 		Grade g2 = new Grade(7, st2, c2);
-		System.out.println(g2);
+		allGrades.add(g2);
+		Grade g3 = new Grade(10, st3, c4);
+		allGrades.add(g3);
+		Grade g4 = new Grade(8, st3, c3);
+		allGrades.add(g4);
 		
-		public static ArrayList<Professor> profLists = new
-				ArrayList<>();
+		for(Grade tempG : allGrades) {
+			System.out.println(tempG);
+		}
+		System.out.println("------------------------------------------");
+		
+		try {
+			System.out.println(st3.getName() + " " + st3.getSurname() + " -> " + calculateAVGForStudent(st3));
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		
+	}
+	public static float calculateAVGForStudent(Student inputstudent) throws Exception{
+		if(inputstudent == null) throw new Exception("Problems with input");
+		
+		float sum = 0;
+		int howMany = 0;
+		
+		for(Grade tempgr : allGrades) {
+			if(tempgr.getStudent().equals(inputstudent)) {
+				sum =sum + tempgr.getValue();
+				howMany++;
+			}
+		}
+		return sum/howMany;
 		
 	}
 }
