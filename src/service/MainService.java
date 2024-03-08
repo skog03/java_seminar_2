0783e00289bf2dab5cd85c4661365c3de6812872
RudolfsTaurins <s22sprokate@venta.lv>
2024-03-08 15:57:00 +0200

    Add: sorter function added

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -21,13 +21,11 @@ public class MainService {
 	
 	public static void main(String[] args) {
 		Professor pr1 = new Professor();
-		allProfessors.add(pr1);
 		Professor pr2 = new Professor("Karina", "Skirmante", Degree.mg);
-		allProfessors.add(pr2);
 		Professor pr3 = new Professor("Marcis", "Naktins", Degree.mg);
-		allProfessors.add(pr3);
 		Professor pr4 = new Professor("Estere", "Vitola", Degree.mg);
-		allProfessors.add(pr4);
+		
+		allProfessors.addAll(Arrays.asList(pr1, pr2, pr3, pr4));
 		
 		//TODO take a look at ArrayList class
 		for(int i = 0; i < allProfessors.size(); i++) {
@@ -39,11 +37,11 @@ public class MainService {
 		
 		
 		Student st1 = new Student();
-		allStudents.add(st1);
 		Student st2 = new Student("Annija", "Aumale");
-		allStudents.add(st2);
 		Student st3 = new Student("Lara", "Bernardes");
-		allStudents.add(st3);
+		Student st4 = new Student("Viktors", "Kokins");
+		
+		allStudents.addAll(Arrays.asList(st1, st2, st3, st4));
 		
 		for(Student tempSt : allStudents) {
 			System.out.println(tempSt);
@@ -51,37 +49,32 @@ public class MainService {
 		System.out.println("------------------------------------------");
 		
 		Course c1 = new Course();
-		
 		Course c2 = new Course("JAVA", 4, pr2);
-		
 		Course c3 = new Course("Networking", 2, pr3);
-		
 		Course c4 = new Course("Data Structures", 2, pr4);
-		Course c5 = new Course("C++", 4, pr4);
+		Course c5 = new Course("Object Oriented Programming", 4, pr4);
 		Course c6 = new Course("Phyton", 4, pr4);
 		
-		
 		allCourses.addAll(Arrays.asList(c1,c2,c3,c4, c5, c6));
+
 		
 		for(Course tempC : allCourses) {
 			System.out.println(tempC);
 		}
 		System.out.println("------------------------------------------");
 		
+		
 		Grade g1 = new Grade();
-		allGrades.add(g1);
 		Grade g2 = new Grade(7, st2, c2);
-		allGrades.add(g2);
 		Grade g3 = new Grade(10, st3, c4);
-		allGrades.add(g3);
 		Grade g4 = new Grade(7, st3, c2);
-		allGrades.add(g4);
 		Grade g5 = new Grade(4, st3, c2);
-		allGrades.add(g5);
 		Grade g6 = new Grade(5, st2, c2);
-		allGrades.add(g6);
 		Grade g7 = new Grade(9, st2, c4);
-		allGrades.add(g7);
+		Grade g8 = new Grade(10, st4, c3);
+		Grade g9 = new Grade(6, st4, c4);
+		
+		allGrades.addAll(Arrays.asList(g1, g2, g3, g4,g5,g6,g7,g8,g9));
 		
 		for(Grade tempG : allGrades) {
 			System.out.println(tempG);
@@ -90,12 +83,35 @@ public class MainService {
 		
 		try {
 			System.out.println("Avg: " + st3.getName() + " " + st3.getSurname() + " -> " + calculateAVGForStudent(st3));
+			System.out.println("Avg: " + st2.getName() + " " + st2.getSurname() + " -> " + calculateAVGForStudent(st2));
+			System.out.println("Avg: " + st4.getName() + " " + st4.getSurname() + " -> " + calculateAVGForStudent(st4));
 			
 			System.out.println("Weighted Avg: " + st3.getName() + " " + st3.getSurname() + " -> " + wheightedAVGgrade(st3));
+			System.out.println("Weighted Avg: " + st2.getName() + " " + st2.getSurname() + " -> " + wheightedAVGgrade(st2));
+			System.out.println("Weighted Avg: " + st4.getName() + " " + st4.getSurname() + " -> " + wheightedAVGgrade(st4));
 			
 			System.out.println("Course Avg: " + c2.getTitle() + " -> " + courseAVG(c2));
+			System.out.println("Course Avg: " + c3.getTitle() + " -> " + courseAVG(c3));
+			System.out.println("Course Avg: " + c4.getTitle() + " -> " + courseAVG(c4));
 			
 			System.out.println("Courses taught by : " + pr4.getName()+ " " + pr4.getSurname() + " -> " + coursesPerProfessor(pr4));
+			System.out.println("Courses taught by : " + pr3.getName()+ " " + pr3.getSurname() + " -> " + coursesPerProfessor(pr3));
+			System.out.println("Courses taught by : " + pr2.getName()+ " " + pr2.getSurname() + " -> " + coursesPerProfessor(pr2));
+			
+			
+			System.out.println("------------------------------------------");
+			
+			for(Student tempst : allStudents) {
+				System.out.println(tempst.getName() + " " + tempst.getSurname() + " -> " + calculateAVGForStudent(tempst));
+			}
+			System.out.println("------------------------------------------");
+			
+			sortStudentsByAVG();
+			
+			for(Student tempst : allStudents) {
+				System.out.println(tempst.getName() + " " + tempst.getSurname() + " -> " + calculateAVGForStudent(tempst));
+			}
+			
 		}
 		catch (Exception e) {
 			System.out.println(e);
@@ -123,6 +139,7 @@ public class MainService {
 		return sum/howMany;
 	}
 	
+	//calculates average grade based on course credit points (weighted average grade) for each student
 	//Java 6 (4cp) datastruct 8(2kp) = ((6* 4KP) + (8* 2KP))/(4KP + 2 KP)
 	
 	public static float wheightedAVGgrade(Student inputStudent) throws Exception {
@@ -140,6 +157,7 @@ public class MainService {
 		return sum/howMany;
 	}
 	
+	// method which calculates average grade for each course
 	
 	public static float courseAVG(Course inputcourse) throws Exception {
 		if(inputcourse == null) throw new Exception("Problems with input");
@@ -171,7 +189,25 @@ public class MainService {
 		return count;
 	}
 	
+	//method which sort all students by their average grade
 	
+	public static void sortStudentsByAVG() throws Exception {
+		
+		for(int i = 0; i < allStudents.size(); i++) {
+			for(int j = 0; j < allStudents.size(); j++){
+				
+				Student tempI = allStudents.get(i);
+				Student tempJ = allStudents.get(j);
+				
+				if(calculateAVGForStudent(tempJ) < calculateAVGForStudent(tempI)) {
+					Student temp = allStudents.get(i);
+					allStudents.set(i, allStudents.get(j));
+					allStudents.set(j, temp);
+				}
+			}
+		}
+		
+	}
 	
 	
 	
